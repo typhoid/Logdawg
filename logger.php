b4e3504ea69b4361496f7f52a0337415d31e451a
typhoid <typhoidfever@californiacolleges.edu>
2016-05-10 16:41:00 -0400

    Update logger.php
    fixed typo, added proper concatenation of strings

--- logger.php
+++ logger.php
@@ -7,40 +7,33 @@ replace were it says .png in the line added to the htmlaccess with whatever exte
  if you're keeping it as it is then save this logger file as .png on the server
 special thanks to ozkar */ 
 extract($_SERVER);
-
-
 //open jpg image and set it as the display for the page. can use other image type but must be different than the extension put into htmlaccess
-$name = 'picturename.jpg';
+$name = 'apache_pb.gif';
 //replace this with file of choice. could be super low quality blurry copy of picturename.jpg or textfile saying preview not available idk
-$zuckerberg = 'fggt.txt';
+$zuckerberg = 'apache_pb2.gif';
 //check to see if facebook is trying to make a preview of the image. we want to show them something else.
 if($HTTP_USER_AGENT =='facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)')
 {
 // change 'r' below to rb if using another picture instead of a text file
-$fp = fopen($zuckerberg,'r');
-
+$fp = fopen($zuckerberg,'rb');
 }
 else {$fp = fopen($name, 'rb');
 }
-
 header("Content-Type: image/png");
 header("Content-Length: " . filesize($name));
 fpassthru($fp);
 flock($fp, 3); 
-
 fclose($fp);
-
-
 //log visitors ip to log.html
 $file = fopen('log.html', 'a');
 //time
-stime = date('H:i dS F');
-fwrite ($file, '<b>Time:</b> $time<br/>');
+$time = date('H:i dS F');
+fwrite ($file, "<b>Time:</b> {$time}  <br/>");
 //ip address
-fwrite($file, '<b> Ip Address:</b> $REMOTE_ADDR<br/>');
+fwrite($file, "<b> Ip Address:</b> {$REMOTE_ADDR}  <br/>");
 //show referer so you can set up multiple pages of these to record to same log and use referer to help identify who you targeted
-fwrite($file, '<b>Referer:</b> $HTTP_REFFERER<br/>');
+fwrite($file, "<b>Referer:</b> {$HTTP_REFFERER} <br/>");
 //browser details
-fwrite($file, '<b>Browser:</b> $HTTP_USER_AGENT<hr/>');
+fwrite($file, "<b>Browser:</b> {$HTTP_USER_AGENT} <hr/>");
 fclose($file);
 ?>
